f1f385c1d14d583978281fd6ba2340900dcd8cdf
Harettini <trol199717@o2.pl>
2020-11-08 15:47:46 +0000

    już nic nie rozumiem

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -3,21 +3,26 @@
 #include "Resource.hpp"
 
 class ResourceManager
-{ Resource* Q;
+{ 
+  Resource* Q;
 
   public:
-     ResourceManager() {Q=new Resource;}
+    ResourceManager() {Q=new Resource;}
     ~ResourceManager() {delete Q;}
     double get()
     {
-        return Q[0].get();
+      return Q[0].get();
     }
-    ResourceManager(const ResourceManager& aa):Q{aa.Q} {}
- ResourceManager& operator=(const ResourceManager& aa) 
- { Q=aa.Q; 
- return *this; }
-ResourceManager (const ResourceManager&& aa):Q{std::move(aa.Q)} {}
-ResourceManager& operator=(ResourceManager&& aa)
-{Q=std::move(aa.Q);
-return *this;}
+    ResourceManager(const ResourceManager& aa) : Q{aa.Q} {}
+    ResourceManager& operator=(const ResourceManager& aa) 
+  {
+    Q = aa.Q; 
+    return *this; 
+  }
+  ResourceManager (const ResourceManager&& aa) : Q{(aa.Q)} {}
+  ResourceManager& operator=(ResourceManager&& aa)
+  {
+    Q = (aa.Q);
+    return *this;
+  }
 };
