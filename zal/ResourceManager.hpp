43c86597a8a507890229e948a6d4b596abd6238c
Harettini <trol199717@o2.pl>
2020-11-09 18:46:40 +0000

    a teraz?

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -7,26 +7,37 @@ class ResourceManager
     Resource* Q;
 
 public:
-    ResourceManager() { Q = new Resource{}; }
+    ResourceManager() { Q=new Resource;}
     ~ResourceManager() { delete Q; }
     double get() { return Q->get(); }
-    ResourceManager(const ResourceManager& aa) : Q{aa.Q} {}
-    ResourceManager& operator=(const ResourceManager& aa)
-    { if(this==&aa) return *this;
-        Q=nullptr;
-        Q = aa.Q;
-        return *this;
-    }
-    ResourceManager(const ResourceManager&& aa) {
+    ResourceManager(const ResourceManager& aa)
+     {
+ Q=new Resource;
+ Q=aa.Q;
 
+     }
+    ResourceManager& operator=(const ResourceManager& aa)
+    { if(this!=&aa)
+        {delete Q;
+        Q=new Resource;
+        Q = aa.Q;}
+        return *this;
+        
+    }
+    ResourceManager(const ResourceManager&& aa) 
+    {
+      Q=new Resource;
       Q=aa.Q;
-      aa.Q=nullptr;
+      aa.Q  =nullptr;
     }
     ResourceManager& operator=(const ResourceManager&& aa)
-    { if(this==&aa) return *this;
-        Q=nullptr;
+    { if(this!=&aa)
+    {   delete Q;
+        Q=new Resource;
         Q=aa.Q;
-        aa.Q=nullptr;
+        aa.Q = nullptr; }
         return *this;
+       
+   
     }
 };
