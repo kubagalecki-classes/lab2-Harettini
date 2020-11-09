3d643ad5d8fd9590950d9cf9787a02ba10ec0a47
Harettini <trol199717@o2.pl>
2020-11-09 19:05:16 +0000

    work, work

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -28,16 +28,14 @@ public:
     {
       Q=new Resource;
       Q=aa.Q;
-      aa.Q  = nullptr;
+      aa.Q  = std::nullptr_t;
     }
     ResourceManager& operator=(const ResourceManager&& aa)
     { if(this!=&aa)
     {   delete Q;
         Q=new Resource;
         Q=aa.Q;
-        aa.Q = nullptr; }
+        aa.Q = std::nullptr_t;} 
         return *this;
-       
-   
     }
 };
